75b243e8fa116954c8155c0b30ca8f5c2bfef595
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-20 17:39:17 +0100

    add the humidity, weather, wind speed

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ $(document).ready(function(){
 		url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=c4ab0f5a04b5d0e07584f005992ab52e"
 	}).then(function(data) {
 		$('#temp').append(data.main.temp);
+		$('#humidity').append(data.main.humidity);
+		$('#desc').append(data.weather[0].description);
+		$('#speed').append(data.wind.speed);
 		$('#pressure').append(data.main.pressure);
+		
 		console.log(data)
 	});
     });
