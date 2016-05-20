3e7123ed4921b16f582969a9267d9f9175e6b3b0
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-20 18:46:01 +0100

    change the append method to text in order to avoid concatenation

--- app.js
+++ app.js
@@ -2,23 +2,30 @@
 
 $(document).ready(function(){
 
-    $("button").on("click", function(){
+    $("#submit").on("click", function(){
+
+    	
+
     	var cityname = $("#cityname").val();
     	getData(cityname);
     	function getData(cityname){
         $.ajax({
 		url: "http://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=c4ab0f5a04b5d0e07584f005992ab52e"
 	}).then(function(data) {
-		$('#temp').append(data.main.temp);
-		$('#humidity').append(data.main.humidity);
-		$('#desc').append(data.weather[0].description);
-		$('#speed').append(data.wind.speed);
-		$('#pressure').append(data.main.pressure);
+
+		$('#temp').text(data.main.temp);
+		$('#humidity').text(data.main.humidity);
+		$('#desc').text(data.weather[0].description);
+		$('#speed').text(data.wind.speed);
+		$('#pressure').text(data.main.pressure);
 		
 		console.log(data)
 	});
 	}
     });
+
+    
+
 });
 
 	
