e2b85779e99656ef4eeef8f7cb0cc6ed826c5033
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-20 17:33:22 +0100

    added the temperature and pressure

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
         $.ajax({
 		url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=c4ab0f5a04b5d0e07584f005992ab52e"
 	}).then(function(data) {
-		//$('date').append(data.id);
-		//$('.greeting-content').append(data.content);
+		$('#temp').append(data.main.temp);
+		$('#pressure').append(data.main.pressure);
 		console.log(data)
 	});
     });
